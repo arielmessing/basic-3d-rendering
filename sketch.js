9d539328c876a3e81a0b0fb1333374b3de002404
Ariel Messing <ariel.messing@gmail.com>
2026-04-03 19:03:35 +0100

    some minor adjustments

--- sketch.js
+++ sketch.js
@@ -1,50 +1,55 @@
 let points = [];
-
-let angle = 0
+let angleX = 0; // Separate angles for better control
+let angleY = 0;
 
 function setup() {
   createCanvas(600, 400);
 
+  // Initialize cube vertices
   points[0] = createVector(-0.5, -0.5, -0.5); 
   points[1] = createVector(0.5, -0.5, -0.5);
   points[2] = createVector(0.5, 0.5, -0.5);
   points[3] = createVector(-0.5, 0.5, -0.5);
-
   points[4] = createVector(-0.5, -0.5, 0.5); 
   points[5] = createVector(0.5, -0.5, 0.5);
   points[6] = createVector(0.5, 0.5, 0.5);
   points[7] = createVector(-0.5, 0.5, 0.5);
 }
 
-function draw() {
-  background(0);
-  translate(width/2, height/2);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
+function draw() {
+  background('#fafaf9');
+  translate(width / 2, height / 2);
+
+  angleY += 0.01;
+  angleX += 0.005;  
+
+  // Define rotation matrices using our new angles
   let rotationX = [
     [1, 0, 0],
-    [0, cos(angle), -sin(angle)],
-    [0, sin(angle), cos(angle)],
+    [0, cos(angleX), -sin(angleX)],
+    [0, sin(angleX), cos(angleX)],
   ];
 
   let rotationY = [
-    [cos(angle), 0, -sin(angle)],
+    [cos(angleY), 0, -sin(angleY)],
     [0, 1, 0],
-    [sin(angle), 0, cos(angle)]
-  ];
-
-  let rotationZ = [
-    [cos(angle), -sin(angle), 0],
-    [sin(angle), cos(angle), 0],
-    [0, 0, 1]
+    [sin(angleY), 0, cos(angleY)]
   ];
 
   let points1 = [];
+  
   for (let p of points) {
     let matrix = vector2Matrix(p);
+    
+    // Apply rotations
     let rotated = matmul(rotationX, matrix);
     rotated = matmul(rotationY, rotated);
-    rotated = matmul(rotationZ, rotated);
 
+    // Perspective Projection Calculation
     let distance = 2;
     let z = 1 / (distance - rotated[2][0]);
 
@@ -54,65 +59,53 @@ function draw() {
     ];
 
     let projected2d = matmul(projection, rotated);
-    
     let p1 = matrix2Vector(projected2d);
     p1.mult(200);
-
     points1.push(p1);
   }
 
-  stroke(255);
-  strokeWeight(1);
+  // Drawing the cube edges
+  stroke('#1a1a1a');
+  strokeWeight(1.5);
   noFill();
   for (let i = 0; i < 4; i++) {
-    let a = points1[i];
-    let b = points1[(i + 1) % 4];
-    let c = points1[i + 4];
-    let d = points1[(i + 1) % 4 + 4];
-
-    line(a.x, a.y, b.x, b.y);
-    line(c.x, c.y, d.x, d.y);
-    line(a.x, a.y, c.x, c.y);
+    // Draw squares
+    line(points1[i].x, points1[i].y, points1[(i + 1) % 4].x, points1[(i + 1) % 4].y);
+    line(points1[i + 4].x, points1[i + 4].y, points1[((i + 1) % 4) + 4].x, points1[((i + 1) % 4) + 4].y);
+    // Connect squares
+    line(points1[i].x, points1[i].y, points1[i + 4].x, points1[i + 4].y);
   }
-
-  angle += 0.01;
 }
 
+// --- HELPER FUNCTIONS ---
+
 function vector2Matrix(vector) {
-  return [[vector.x], [vector.y] ,[vector.z]];
+  return [[vector.x], [vector.y], [vector.z]];
 }
 
 function matrix2Vector(matrix) {
-  let vector = 
-    createVector(
-      matrix[0][0], 
-      matrix[1][0],
-      matrix.length > 2 ? matrix[2][0] : undefined);
-
-  return vector;
+  return createVector(
+    matrix[0][0], 
+    matrix[1][0],
+    matrix.length > 2 ? matrix[2][0] : undefined
+  );
 }
 
 function matmul(a, b) {
   let rowsA = a.length;
   let colsA = a[0].length;
-  let rowsB = b.length;
   let colsB = b[0].length;
-
   let res = [];
-
   for (let i = 0; i < rowsA; i++) {
     let resRow = [];
     for (let j = 0; j < colsB; j++) {
       let sum = 0;
-
       for (let k = 0; k < colsA; k++) {
         sum += a[i][k] * b[k][j];
       }
-      
       resRow.push(sum);
     }
     res.push(resRow);
   }
-
   return res;
 }
